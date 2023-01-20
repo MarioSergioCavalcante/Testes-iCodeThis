ebaade76037d3659bc1e0a5a31b44092757316e7
MarioSergioCavalcante <marioffcavalcante@gmail.com>
2023-01-20 17:15:57 -0300

    Upload do challenge 19/01/2023

--- Ex-19jan23/script.js
+++ Ex-19jan23/script.js
@@ -1,6 +1,15 @@
-
-
-
+const cartao = document.querySelector('#cartao--claro');
+const triangulo= document.querySelector('#triangulo--claro');
 function Teste(){
-    alert("Teste")
+    console.log('Teste');
+    cartao.classList.toggle('esconder');
+    console.log(cartao.classList);
+    triangulo.classList.toggle('esconder');
+}
+
+function toggleVisualizacaoEscuro(){
+    const cartao2 = document.querySelector('.cartao--escuro');
+    const triangulo= document.querySelector('#triangulo--escuro');
+    cartao2.classList.toggle('esconder');
+    triangulo.classList.toggle('esconder');
 }
